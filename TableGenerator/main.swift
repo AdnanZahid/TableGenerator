e5ac1bc28a315054d0ffd5622a56aa3bcaabbd4e
Adnan Zahid <adnaan.zaahid@gmail.com>
2018-05-02 15:35:26 +0500

    This program asks you times table questions and marks you accordingly.
    This program asks you times table questions and marks you accordingly.

--- TableGenerator/main.swift
+++ TableGenerator/main.swift
@@ -8,5 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+var total = 0
+var correct = 0
+var wrong = 0
 
+while true {
+    let aLowerLimit = 2
+    let aUpperLimit = 9
+    let aDifference = UInt32(aUpperLimit + 1 - aLowerLimit)
+    
+    let bLowerLimit = 11
+    let bUpperLimit = 100
+    let bDifference = UInt32(bUpperLimit + 1 - bLowerLimit)
+    
+    let a = aLowerLimit + Int(arc4random_uniform(aDifference))
+    let b = bLowerLimit + Int(arc4random_uniform(bDifference))
+    let c = a * b
+    print("\(a) x \(b) = ", separator: "", terminator: "")
+    let answer = readLine(strippingNewline: true)
+    
+    if answer == "q" {
+        print("Correct: \(correct)/\(total)")
+        print("Wrong: \(wrong)/\(total)")
+        break
+    } else if Int(answer ?? "") == c {
+        print("Correct!")
+        correct += 1
+    } else {
+        print("Wrong! Correct answer is \(c)")
+        wrong += 1
+    }
+    
+    total += 1
+}
